132b30e611586aec8201575b8ee32ea53c5217a8
EzeClavijo <ezequielclavijo5@gmail.com>
2023-11-07 18:51:22 -0300

    actualización de estructura

--- js/app.js
+++ js/app.js
@@ -20,12 +20,12 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
       const carElement = `
           <div class="row">
               <div class="col-12 col-lg-4 mb-3">
-                  <img class="auto img-fluid" src="${car.image}" alt="" />
+                  <img class="car img-fluid" src="${car.image}"  alt="" />
               </div>
               <div class="col-12 col-lg-8">
-                  <h4>${car.model}</h4>
-                  <div class="d-flex justify-content-end">
-                      <h5>
+                  <h4 class="position-absolute" >${car.model}</h4>
+                  <div class="d-flex justify-content-end  >
+                      <h5 class="stars">
                           ${car.year} | $USD${car.price_usd} |
                           <i class="bi bi-star-fill text-warning"></i>
                           <i class="bi bi-star-fill text-warning"></i>
@@ -34,19 +34,20 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
                           <i class="bi bi-star text-warning"></i>
                       </h5>
                   </div>
-                  <p>${car.description}</p>
-                  <button type="submit" id="shopingCart">
+                  <p class="py-2">${car.description}</p>
+                  <button type="submit" class="btn btn-success" id="comprar">
                       <i class="bi bi-cart3"></i>
                       Comprar
                   </button>
-                  <button type="submit" id="infoCart">
+                  <button type="submit" class="btn btn-light">
                       <i class="bi bi-plus-square"></i> Más información
                   </button>
-                  <button type="submit" id="shareCart">
+                  <button type="submit" class="btn btn-light">
                       <i class="bi bi-box-arrow-up-right"></i> Compartir
                   </button>
               </div>
           </div>
+          <hr />
       `;
       prueba.insertAdjacentHTML("beforeend", carElement);
     }
