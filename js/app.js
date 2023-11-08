9319f9af72bb0521b1ba929db1c7ccaaed6d6142
Nicolás Dos Santos <disney2santos@gmail.com>
2023-11-08 00:43:10 -0300

    Función de filtro añadido

--- js/app.js
+++ js/app.js
@@ -44,38 +44,7 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
   })
   .then(function (cars) {
     for (let car of cars) {
-      const carElement = `
-          <div class="row">
-              <div class="col-12 col-lg-4 mb-3">
-                  <img class="car img-fluid" src="${car.image}"  alt="" />
-              </div>
-              <div class="col-12 col-lg-8">
-                  <h4 class="position-absolute" >${car.model}</h4>
-                  <div class="d-flex justify-content-end  >
-                      <h5 class="stars">
-                          ${car.year} | $USD${car.price_usd} |
-                          <i class="bi bi-star-fill text-warning"></i>
-                          <i class="bi bi-star-fill text-warning"></i>
-                          <i class="bi bi-star-fill text-warning"></i>
-                          <i class="bi bi-star-fill text-warning"></i>
-                          <i class="bi bi-star text-warning"></i>
-                      </h5>
-                  </div>
-                  <p class="py-2">${car.description}</p>
-                  <button type="submit" class="btn btn-success" id="comprar">
-                      <i class="bi bi-cart3"></i>
-                      Comprar
-                  </button>
-                  <button type="submit" class="btn btn-light">
-                      <i class="bi bi-plus-square"></i> Más información
-                  </button>
-                  <button type="submit" class="btn btn-light">
-                      <i class="bi bi-box-arrow-up-right"></i> Compartir
-                  </button>
-              </div>
-          </div>
-          <hr />
-      `;
+      const carElement = buildCarDivs(car)
       prueba.insertAdjacentHTML("beforeend", carElement);
     }
     console.log("hola");
@@ -83,3 +52,98 @@ fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
   .catch((error) => {
     console.error("Error al obtener los datos: " + error);
   });
+
+
+  const searchButton = document.querySelector('#searchButton');
+
+  searchButton.addEventListener('click', filterCars);
+
+  function filterCars (){
+    event.preventDefault();
+
+    const carContainer = document.querySelector('#prueba');
+    carContainer.innerHTML = "";
+
+    let carDiv = "";
+
+
+    const year = document.querySelector('#seleccionAnio');
+    const marca = document.querySelector('#seleccionMarca');
+    const modelo = document.querySelector('#seleccionModelo');
+    const status = document.querySelector('#selectStatus');
+
+    const yearValue = year.value;
+    const marcaValue = marca.value;
+    const modeloValue = modelo.value;
+    const statusValue = status.value;
+
+    console.log(yearValue);
+    console.log(marcaValue);
+    console.log(modeloValue);
+    console.log(statusValue);
+
+    url = 'https://ha-front-api-proyecto-final.vercel.app/cars';
+
+    fetch(url)
+    .then(function(response){
+      if(response.ok){
+        return response.json();
+      }
+    }).then(function(data){
+      data.forEach(element => {
+
+        if (
+          (yearValue === "1" || element.year == yearValue) &&
+          (marcaValue === "1" || element.brand == marcaValue) &&
+          (modeloValue === "1" || element.model == modeloValue) &&
+          (statusValue === "1" || element.status == statusValue)
+        ) {
+          carDiv= buildCarDivs(element);
+          prueba.insertAdjacentHTML("beforeend", carDiv);
+        } 
+        
+      });
+      if(carDiv == ""){
+        alert ("No se ha encontrado ningún vehiculo para los filtros seleccionados");
+      }
+    })
+
+  }
+
+
+  function buildCarDivs(element){
+    const carDiv = `
+    <div class="row">
+        <div class="col-12 col-lg-4 mb-3">
+            <img class="car img-fluid" src="${element.image}"  alt="" />
+        </div>
+        <div class="col-12 col-lg-8">
+            <h4 class="position-absolute" >${element.model}</h4>
+            <div class="d-flex justify-content-end  >
+                <h5 class="stars">
+                    ${element.year} | $USD${element.price_usd} |
+                    <i class="bi bi-star-fill text-warning"></i>
+                    <i class="bi bi-star-fill text-warning"></i>
+                    <i class="bi bi-star-fill text-warning"></i>
+                    <i class="bi bi-star-fill text-warning"></i>
+                    <i class="bi bi-star text-warning"></i>
+                </h5>
+            </div>
+            <p class="py-2">${element.description}</p>
+            <button type="submit" class="btn btn-success" id="comprar">
+                <i class="bi bi-cart3"></i>
+                Comprar
+            </button>
+            <button type="submit" class="btn btn-light">
+                <i class="bi bi-plus-square"></i> Más información
+            </button>
+            <button type="submit" class="btn btn-light">
+                <i class="bi bi-box-arrow-up-right"></i> Compartir
+            </button>
+        </div>
+      </div>
+      <hr />
+      `;
+
+      return carDiv;
+  }
